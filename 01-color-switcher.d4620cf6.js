8ca9d8d294f4a5dc1ac349bb6adb95cab698b25f
PavloShyshko <111237173+PavloShyshko@users.noreply.github.com>
2023-01-29 22:00:32 +0000

    Deploying to gh-pages from @ PavloShyshko/PavloShyshko-goit-js-hw-09@3633a72defad7a278fcb888c1bcb29c33e1af562 🚀

---
+++ 01-color-switcher.d4620cf6.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(){a.disabled=!1,e.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3)})),a.addEventListener("click",(function(){a.disabled=!0,e.disabled=!1,clearInterval(t)})),a.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.d4620cf6.js.map
